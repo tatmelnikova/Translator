a20f63f1083fc31ec3b1e029e29e15207570b681
Tatyana Kazmina <tatmelnikova@gmail.com>
2017-03-22 12:23:09 +0300

    language model

---
+++ app/src/main/java/kazmina/testapp/translator/Language.java
@@ -0,0 +1,27 @@
+package kazmina.testapp.translator;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * Created by Fina on 21.03.2017.
+ */
+
+public class Language {
+
+
+    private String[] dirs;
+    protected LinkedHashMap<String, String> langs;
+
+    public LinkedHashMap<String, String> getLangs() {
+        return langs;
+    }
+
+    public void setLangsMap(LinkedHashMap<String, String> langs) {
+        this.langs = langs;
+    }
+
+    public void setDirs(String[] dirs){this.dirs = dirs;}
+    public String[] getDirs(){return dirs;}
+
+}
