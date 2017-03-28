70558438231faec69bc5ba46da044a65ddd9804c
Tatyana Kazmina <tatmelnikova@gmail.com>
2017-03-28 15:02:41 +0300

    получение результатов перевода в YandexTranslateApi

--- app/src/main/java/kazmina/testapp/translator/YandexTranslateApi.java
+++ app/src/main/java/kazmina/testapp/translator/YandexTranslateApi.java
@@ -11,6 +11,22 @@ import retrofit2.http.Query;
  */
 
 interface YandexTranslateApi {
+    /**
+     * @param currentLangID - текущий язык локали
+     * @return LanguageLocalisation - список кодов языков с переводами для заданной локали
+     */
     @GET("/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20170320T074137Z.b79dbc842fbc5526.fd9448c978b6f6ad53587000adc0d56613cd4358")
     Call<LanguageLocalisation> getLanguages(@Query("ui") String currentLangID);
+
+
+    /**
+     * @param text - текст, который нужно перевести
+     * @param direction - напрпавление перевода
+     * @return
+     */
+    @GET("api/v1.5/tr.json/translate?key=trnsl.1.1.20170320T074137Z.b79dbc842fbc5526.fd9448c978b6f6ad53587000adc0d56613cd4358")
+    Call<TranslateResult> getTranslate(
+            @Query("text") String text,
+            @Query("lang") String direction
+    );
 }
