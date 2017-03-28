7d29d460e97b9c783b2f69771c8b45bd6eb0bc7f
Tatyana Kazmina <tatmelnikova@gmail.com>
2017-03-28 15:03:23 +0300

    добавлен класс TranslateResult для хранения результата перевода

---
+++ app/src/main/java/kazmina/testapp/translator/TranslateResult.java
@@ -0,0 +1,35 @@
+package kazmina.testapp.translator;
+
+/**
+ * класс для хранения результата перевода
+ */
+
+public class TranslateResult {
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String[] getText() {
+        return text;
+    }
+
+    public void setText(String[] text) {
+        this.text = text;
+    }
+
+    private String lang;
+    private int code;
+    private String[] text;
+}
