c60fda13c9a4a472acf4c3e6ef0d4464b39adbf9
Tatyana Kazmina <tatmelnikova@gmail.com>
2017-03-29 14:21:08 +0300

    удален неиспользуемый член класса dirs в LanguageLocalisation

--- app/src/main/java/kazmina/testapp/translator/LanguageLocalisation.java
+++ app/src/main/java/kazmina/testapp/translator/LanguageLocalisation.java
@@ -1,27 +1,18 @@
 package kazmina.testapp.translator;
 
 import java.util.LinkedHashMap;
-import java.util.List;
-
 /**
  * класс для хранения списка доступных языков с переводами их названий
  */
 
 class LanguageLocalisation {
-
-
-    private String[] dirs;
     private LinkedHashMap<String, String> langs;
 
-     LinkedHashMap<String, String> getLangs() {
+    LinkedHashMap<String, String> getLangs() {
         return langs;
     }
 
     public void setLangsMap(LinkedHashMap<String, String> langs) {
         this.langs = langs;
     }
-
-    public void setDirs(String[] dirs){this.dirs = dirs;}
-    public String[] getDirs(){return dirs;}
-
 }
