53cf056f1d4249ecd657b0264710401c32d6d947
Tatyana Kazmina <tatmelnikova@gmail.com>
2017-03-29 14:21:27 +0300

    javadoc для YandexTranslateApi

--- app/src/main/java/kazmina/testapp/translator/YandexTranslateApi.java
+++ app/src/main/java/kazmina/testapp/translator/YandexTranslateApi.java
@@ -22,7 +22,7 @@ interface YandexTranslateApi {
     /**
      * @param text - текст, который нужно перевести
      * @param direction - напрпавление перевода
-     * @return
+     * @return результат перевода
      */
     @GET("api/v1.5/tr.json/translate?key=trnsl.1.1.20170320T074137Z.b79dbc842fbc5526.fd9448c978b6f6ad53587000adc0d56613cd4358")
     Call<TranslateResult> getTranslate(
