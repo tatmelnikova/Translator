ef5745e3dadcdbf100ee787c27d160ad518bd992
Tatyana Kazmina <tatmelnikova@gmail.com>
2017-03-30 15:51:33 +0300

    в TranslatorDBHelper добавлено создание таблицы избранного и представления, для выборки истории и ИД избранного вместе

--- app/src/main/java/kazmina/testapp/translator/db/TranslatorDBHelper.java
+++ app/src/main/java/kazmina/testapp/translator/db/TranslatorDBHelper.java
@@ -3,6 +3,7 @@ package kazmina.testapp.translator.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * хелпер для работы с базой данных
@@ -11,19 +12,50 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class TranslatorDBHelper extends SQLiteOpenHelper implements DBContract{
     private static final int DB_VERSION = 1;
 
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        /*db.execSQL("DROP TABLE " + HISTORY);
+        db.execSQL("DROP TABLE " + FAVORITES);
+        onCreate(db);
+        */
+    }
+
     public TranslatorDBHelper (Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(
-            "CREATE TABLE " + HISTORY + "(" +
-            History.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            History.TEXT + " STRING NOT NULL, " +
-            History.RESULT + " STRING NOT NULL, " +
-            History.DIRECTION_FROM + " STRING NOT NULL, " +
-            History.DIRECTION_TO + " STRING NOT NULL " +
-        ")");
+        String historySql = "CREATE TABLE " + HISTORY + "(" +
+                History.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                History.TEXT + " STRING NOT NULL, " +
+                History.RESULT + " STRING NOT NULL, " +
+                History.DIRECTION_FROM + " STRING NOT NULL, " +
+                History.DIRECTION_TO + " STRING NOT NULL " +
+                ")";
+        db.execSQL(historySql);
+        String favSql =
+                "CREATE TABLE " + FAVORITES + "(" +
+                        Favorites.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        Favorites.TEXT + " STRING NOT NULL, " +
+                        Favorites.RESULT + " STRING NOT NULL, " +
+                        Favorites.DIRECTION_FROM + " STRING NOT NULL, " +
+                        Favorites.DIRECTION_TO + " STRING NOT NULL " +
+                        ")";
+        db.execSQL(favSql);
+
+        String sql = ("CREATE VIEW " + HISTORY_WITH_FAV + " AS SELECT "+
+                HISTORY + "." + History.ID + ", " +  HISTORY + "." + History.TEXT + ", " +  HISTORY + "." + History.RESULT + ", " +
+                HISTORY + "." + History.DIRECTION_FROM + ", " +  HISTORY + "." +  History.DIRECTION_TO + ", " +
+                FAVORITES + " . " + Favorites.ID + " as " + History.FAV_ID +
+                " FROM " + HISTORY +
+                " LEFT JOIN " + FAVORITES +
+                " ON " + HISTORY + "." + History.TEXT + " = " + FAVORITES + "." + Favorites.TEXT + " AND " +
+                HISTORY + "." + History.DIRECTION_FROM  + " = " + FAVORITES + "." + Favorites.DIRECTION_FROM + " AND " +
+                HISTORY + "." + History.DIRECTION_TO + " = " + FAVORITES + "." + Favorites.DIRECTION_TO);
+        Log.d("db", sql);
+        db.execSQL(sql);
+
     }
 
     @Override
